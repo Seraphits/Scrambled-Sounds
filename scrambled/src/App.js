fbc989015c6b0b0b704a9d52b5ba8a62cc41eadd
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-01-12 20:20:59 -0500

    Added route for home page

--- scrambled/src/App.js
+++ scrambled/src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
+// import react, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './Components/05-pages/Home/HomePage.Component';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Scrambled Sounds</h1> 
+    <div>
+       <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/home' component={HomePage} />
+      </Switch>
     </div>
-  );
+  )
 }
 
+
 export default App;
+// QuTypesPage
+// TablaLearnPage
