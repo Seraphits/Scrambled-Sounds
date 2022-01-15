cfb7cbf6c7514e006e8d285c538f7433add91c92
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-01-15 12:15:14 -0500

    Page set up

---
+++ scrambled/src/Components/03-organisms/Static/Hero/HeroHome.Component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './HeroHome.styles.scss';
+
+const HeroHome = () => {
+    return (
+       <div>
+            <h1>HeroHome</h1>
+        </div> 
+    );
+};
+
+export default HeroHome;
